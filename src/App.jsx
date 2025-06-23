5cbabd7a2324d91d1da3fba61b28fc8026eb4287
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-06-23 23:39:26 +0800

    feat: Use hash router instead of browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import LoginPage from './pages/LoginPage.jsx';
 import LandingPage from './pages/LandingPage.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { HashRouter, Routes, Route } from 'react-router';
 import './styles/App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/landing" element={<LandingPage />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
