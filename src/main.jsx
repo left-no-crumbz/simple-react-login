8cf7afdb997f4ea9abe4cf0037b041db72edc841
crumbz <133969306+left-no-crumbz@users.noreply.github.com>
2025-06-24 00:03:09 +0800

    feat: Remove HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router';
-
 import './styles/index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 )
